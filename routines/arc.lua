eb593163f6044cd51383e72b387a74201dcb079f
andr-ew <andrewcshike@gmail.com>
2022-11-21 00:54:35 -0600

    _arc.control, _arc.option

--- routines/arc.lua
+++ routines/arc.lua
@@ -2,7 +2,7 @@
 
 local _arc = {}
 
---utility: loop over a range of leds on the arc using a for loop
+--utility: iterate over a range of leds on the arc using a for loop
 local function ring_range(x1, x2)
     local i, x = 0, x1 - 1
     if x2 <= x1 then x2 = x2 + 64 - 1 end
@@ -64,7 +64,7 @@ do
     end
 end
 
--- number. a decimal controlled by arc roatation, one full rotation of of the indicator is equal to the value of cycle. the range of value may be finite or infinite (math.huge).
+-- number. decimal number + 360 indicator. one full rotation of of the indicator is equal to the value of cycle.
 do
     local defaults = {
         state = {0},
@@ -104,7 +104,7 @@ do
                 end
             elseif crops.mode == 'redraw' then
                 local a = crops.handler
-                local v = crops.get_state(props.state)
+                local v = crops.get_state(props.state) or 0
 
                 local range = props.x[2] - props.x[1] + 1
                 if props.x[1] >= props.x[2] then range = 64 + range - 1 end
@@ -121,6 +121,137 @@ do
                         (i >= scale and i <= scale + props.indicator - 1) and 2 or 1
                     ]
                     if lvl>0 then a:led(props.n, x, lvl) end
+                end
+            end
+        end
+    end
+end
+
+local cs = require 'controlspec'
+
+--control. decimal mapped to a controlspec + 'fader' display
+do
+    local defaults = {
+        state = {0},
+        n = 1,                      --ring index, 1-4
+        x = { 42, 24 },             --start & endpoint led indices. table of 2 ints 1-64
+        levels = { 0, 4, 15 },      --brightness levels, table of three ints 0-15
+                                    --    (levels: background, fill, indicator)
+        controlspec = cs.new(),     --all other properties detirmined by the controlspec
+    }
+    defaults.__index = defaults
+
+    function _arc.control(props)
+        if crops.device == 'arc' then
+            setmetatable(props, defaults)
+
+            if crops.mode == 'input' then
+                local n, d = table.unpack(crops.args)
+
+                if n == props.n then
+                    local old = crops.get_state(props.state) or 0
+                    local v = props.controlspec:unmap(old) + (d * props.controlspec.quantum)
+
+                    if props.controlspec.wrap then
+                        while v > 1 do v = v - 1 end
+                        while v < 0 do v = v + 1 end
+                    end
+                    
+                    v = props.controlspec:map(util.clamp(v, 0, 1))
+                    if old ~= v then
+                        crops.dirty.arc = true
+                        crops.set_state(props.state, v)
+                    end
+                end
+            elseif crops.mode == 'redraw' then
+                local a = crops.handler
+                local v = crops.get_state(props.state) or 0
+
+                local range = props.x[2] - props.x[1] + 1
+                if props.x[1] >= props.x[2] then range = 64 + range - 1 end
+
+                v = math.floor(props.controlspec:unmap(v) * range) + 1
+ 
+                for i, x in ring_range(props.x[1], props.x[2]) do
+                    local l, m = 0, util.linlin(
+                        1, range, props.controlspec.minval, props.controlspec.maxval, i
+                    )
+                    if i == v then l = 2
+                    elseif i > v and m <= 0 then l = 1
+                    elseif i < v and m >= 0 then l = 1 end
+                    
+                    local lvl = props.levels[l + 1]
+                    if lvl>0 then a:led(props.n, x, lvl) end
+                end
+            end
+        end
+    end
+end
+
+--option. integer number + 'tab' display
+do
+    local defaults = {
+        state = {1},
+        n = 1,                      --ring index, 1-4
+        x = { 33, 33 },             --start & endpoint led indices. table of 2 ints 1-64
+        levels = { 4, 15 },         --brightness levels, table of two ints 0-15
+        sensitivity = 0.25,         --input sensitivity / incriment for each enc delta
+        options = 4,                --number of tabs / values
+        wrap = false,               --wrap value around min/max
+        size = nil,                 --number of leds in tabs. nil (auto), single value, or table
+        margin = 0,                 --margin between tabs
+    }
+    defaults.__index = defaults
+
+    function _arc.option(props)
+        if crops.device == 'arc' then
+            setmetatable(props, defaults)
+
+            if crops.mode == 'input' then
+                local n, d = table.unpack(crops.args)
+
+                if n == props.n then
+                    local old = crops.get_state(props.state) or 1
+                    local v = old + (d * props.sensitivity)
+                    local min, max = 1, props.options + 1 - props.sensitivity
+
+                    if props.wrap then
+                        while v > max do v = v - (max - min) end
+                        while v < min do v = v + (max - min) end
+                    end
+ 
+                    v = util.clamp(v, min, max)
+                    if old ~= v then
+                        crops.dirty.arc = true
+                        crops.set_state(props.state, v)
+                    end
+                end
+            elseif crops.mode == 'redraw' then
+                local a = crops.handler
+                local v = crops.get_state(props.state) or 0
+                
+                local count = props.x[2] - props.x[1]
+                if props.x[1] >= props.x[2] then count = 64 + count end
+
+                local options = props.options
+                local vr = math.floor(v)
+                local margin = props.margin
+                local stab = type(props.size) == 'table'
+                local size = props.size or (count/options - props.margin)
+
+                local st = 0
+                for i = 1, options do
+                    local sel = vr == i
+                    local l = sel and 2 or 1
+                    local sz = (stab and size[i] or size)
+
+                    local lvl = props.levels[l]
+
+                    if lvl > 0 then for j = st, st + sz - 1 do
+                        a:led(props.n, math.floor(j + props.x[1]) % 64, lvl)
+                    end end
+                    
+                    st = st + sz + margin
                 end
             end
         end
